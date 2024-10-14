c6c601908436308c037bc113f5ee4dd463d58685
ElopingDev <elopingbuisness@gmail.com>
2024-10-14 17:04:32 +0200

    forgot to remove useless shit in MeowDB.cpp

--- MeowDB.cpp
+++ MeowDB.cpp
@@ -4,17 +4,8 @@
 #include "BankAccount.h"
 
 int main() {
-
+    
     interactiveMenu();
     
-    //Deserialize from file
-    std::ifstream inFile("MeowDB.meow");
-    BankAccount loadedAccount = meowDeserialize(inFile);
-    inFile.close();
-
-    std::cout << "Loaded MeowDB.meow" << std::endl;
-    std::cout << "Account ID: " << loadedAccount.account_id << std::endl;
-    std::cout << "Account Name: " << loadedAccount.account_name << std::endl;
-    std::cout << "Balance: " << loadedAccount.balance << std::endl;
     return 0;
 }
